13fad2d25e18e3bca54ca52d43e83f8532f7563c
ab-wap <abrhammerkuz@gmail.com>
2025-07-17 23:23:50 +0300

    changed the stylings, modified url path

--- toDoApp/urls.py
+++ toDoApp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login_view, name="login"),
+    path('login/', views.login_view, name="login"),
     path('signup/', views.signup_view, name="signup"),
     path('logout/', views.logout_view, name="logout"),
-    path('home/', views.home_view, name="home"),
+    path('', views.home_view, name="home"),
     path('add/', views.add_view, name="add"),
     path('delete/<int:todo_id>/', views.delete_view, name="delete"),
     path('update/<int:todo_id>/', views.update_view, name="update"),
